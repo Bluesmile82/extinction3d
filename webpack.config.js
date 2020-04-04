7179728065a1f38b58a030f418a5854f3fe59689
Alvaro Leal <pursuance@gmail.com>
2020-04-04 14:24:57 +0200

    Add Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 // Webpack uses this to work with directories
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // This is main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
   // Path to your entry point. From this file Webpack will begin his work
   entry: './src/index.js',
-
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
@@ -38,8 +38,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
   resolve: {
     extensions: ['.js', '.jsx', '.scss'],
   },
-  mode: 'development',
+  mode: 'production'
 };
